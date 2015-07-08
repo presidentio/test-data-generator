1b2cb094d62f555c4f034ac0651a643952e28015
presidentio <president603@gmail.com>
2015-07-08 09:35:01 +0300

    fix async problem

--- src/main/java/com/presidentio/testdatagenerator/provider/CounterProvider.java
+++ src/main/java/com/presidentio/testdatagenerator/provider/CounterProvider.java
@@ -2,9 +2,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,26 +43,29 @@ public class CounterProvider implements ValueProvider {
     @Override
     public Object nextValue(Context context, Field field) {
         Map<String, Object> vars = context.getVariables();
-        Long varVal = Long.valueOf(vars.get(var).toString());
-        if (varVal == null) {
-            varVal = 0L;
-        }
-        Long newVal;
+        Long varVal;
+        synchronized (context.getVariables()) {
+            varVal = Long.valueOf(vars.get(var).toString());
+            if (varVal == null) {
+                varVal = 0L;
+            }
+            Long newVal;
 
-        switch (op) {
-            case INC:
-                newVal = varVal + 1;
-                break;
-            case DEC:
-                newVal = varVal - 1;
-                break;
-            case EQ:
-                newVal = varVal;
-                break;
-            default:
-                throw new RuntimeException("Unknown operation: " + op);
+            switch (op) {
+                case INC:
+                    newVal = varVal + 1;
+                    break;
+                case DEC:
+                    newVal = varVal - 1;
+                    break;
+                case EQ:
+                    newVal = varVal;
+                    break;
+                default:
+                    throw new RuntimeException("Unknown operation: " + op);
+            }
+            vars.put(var, newVal);
         }
-        vars.put(var, newVal);
         return TypeConverter.convert(varVal.toString(), field.getType());
     }
 }
